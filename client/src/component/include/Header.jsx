0c3198ea7ae1fc13071a39a569365ff2a21467bb
ssn <snsong91@gmail.com>
2024-07-26 15:58:58 +0900

    App.js 수정 라우터 위치 변경 및 include 폴더 생성

---
+++ client/src/component/include/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header () {
+
+    return(
+        <header>
+            <div>
+                <a href="/">여기모여</a>
+            </div>
+        </header>
+    );
+}
+export default Header;
