8ca93b42e993bb9e5b4407626244e105c518a997
ssn <snsong91@gmail.com>
2024-08-13 18:56:52 +0900

    일기예보 기능 구현 중 및 프론트 서비스 폴더 생성

---
+++ client/src/components/weather/Weather.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import { axiosGetWaetherData } from "../../services/WeatherService";
+
+function Weather () {
+    useEffect(() =>{
+        axiosGetWaetherData();
+    },[]);
+
+
+    return(
+        <>
+        
+        </>
+    );
+}
+
+export default Weather;
--- server/user/src/main/java/com/trip/user/HomeController.java
+++ server/user/src/main/java/com/trip/user/HomeController.java
@@ -1,13 +1,20 @@
 package com.trip.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.trip.weather.WeatherController;
+import com.trip.weather.WeatherService;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Controller
 public class HomeController {
+	
+	@Autowired
+	WeatherService weatherService;
 
 	@GetMapping("/")
 	public String home() {
